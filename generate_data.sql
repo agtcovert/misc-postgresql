b5bccdb1a3d5fc63b9beead954765f1ad6f0cb1f
Eric Pierce <agtcovert@gmail.com>
2017-04-11 20:24:11 -0400

    Add helper functions

---
+++ generate_data.sql
@@ -0,0 +1,26 @@
+CREATE OR REPLACE FUNCTION generate_data(rowcount BIGINT)
+RETURNS VOID AS $$
+DECLARE
+    i BIGINT := 0;
+    firstname VARCHAR(100);
+BEGIN
+  FOR i in 1..rowcount
+  LOOP
+
+    firstname := random_string(25);
+
+    INSERT INTO normal_data
+      (id, firstname, lastname, email, last_update)
+    VALUES
+      (gen_random_uuid(), firstname, random_string(35),
+       firstname || i || '@' ||
+          (CASE (random() * 2)::INTEGER
+            WHEN 0 THEN 'gmail'
+            WHEN 1 THEN 'yahoo'
+            WHEN 2 THEN 'outlook'
+           END) || '.com'
+        , now());
+  END LOOP;
+  RETURN;
+END;
+$$ LANGUAGE 'plpgsql';
